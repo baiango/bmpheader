5aafea2e28b86f98e499810b2e01f44821b55b05
baiango <105705580+baiango@users.noreply.github.com>
2023-11-04 23:42:50 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Bmpheader
+The first library known that will display your algorithm(e.g. Perlin) in 8-bit RGBA channels, and it doesn't require a Ph.D. in mathematics to understand it.  
+Made in C++14.  
+
+## Usage
+You can set the pixel by gray, Col8, or r, g, b, and a uint8_t with set_pixel(),  
+or set the color cursor position by set_data_cursor_position() or xy() and chain it with r(), g(), b(), or a(),  
+or you can set it by reference from "*px_ptr(x, y)".  
+
+The Col8 is a union. To set the color with .rgba, use this format "0xrrggbbaa".  
+You can do it with .r, .g, .b, and .a as well.
+```
+class Bmp {
+public:
+	union Col8 {
+		struct {
+			uint8_t r;
+			uint8_t	g;
+			uint8_t b;
+			uint8_t a;
+		};
+		uint32_t rgba = 0x202020ff;
+	};
+}
+```
+
+```
+#include "bmpheader.hpp"
+
+
+class Bmp {
+public:
+	void save_sample(std::string file_name = "sample.bmp") {
+		for (int32_t x = 0; x < get_width(); x++)
+			for (int32_t y = 0; y < get_height(); y++)
+				*px_ptr(x, y) = {uint8_t(x + y), 50, uint8_t(y * 0.3)};
+
+		save(file_name);
+	}
+}
+
+
+void unit_test() {
+	{
+		Bmp bmp(512, 512);
+
+		bmp.save_sample();
+
+		bmp.clear_pixel();
+		bmp.read("sample.bmp");
+		bmp.save("read.bmp");
+	}
+
+	for (int32_t count = 20; count <= 45; count += 1) {
+		Bmp bmp(count, count);
+
+		for (int32_t x = 10; x < std::min(30, bmp.get_width()); x++)
+			for (int32_t y = 10; y < std::min(30, bmp.get_height()); y++)
+				bmp.xy(x, y)
+				.r(x)
+				.g(y * 10)
+				.b(x * 30);
+
+		bmp.save(std::to_string(count) + ".bmp");
+	}
+}
+```
---
+++ bmpheader.hpp
@@ -0,0 +1,224 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cassert>
+
+
+// You can set the pixel by gray, Col8, or r, g, b, and a uint8_t with set_pixel(),
+// or set the color cursor position by set_data_cursor_position() or xy() and chain it with r(), g(), b(), or a(),
+// or you can set it by reference from "*px_ptr(x, y)".
+//
+// The Col8 is a union. To set the color with .rgba, use this format "0xrrggbbaa".
+// You can do it with .r, .g, .b, and .a as well.
+
+
+class Bmp {
+public:
+	// Tell the compiler do not pad the struct with "#pragma pack(push, 1)"!
+#pragma pack(push, 1)
+	struct Header {
+		// The reason not to use std::string is,
+		// it add extra stuff to the header, so it will corrupt the header
+		char signature[2] = {'B', 'M'};
+		uint32_t size = 512 * 512 + 54;
+		uint16_t reserved0 = 0;
+		uint16_t reserved1 = 0;
+		uint32_t pixel_data_offset = 54;
+
+		struct Info {
+			uint32_t byte_size = 40;
+			int32_t width = 512;
+			int32_t height = 512;
+			uint16_t color_plane_count = 1;
+			// Most decoder can't read 3 channels, and it's buggy
+			// So I made it 4 channels instead
+			uint16_t color_depth = 8 * 4;
+			uint32_t compress_method = 0;
+			uint32_t raw_bitmap_size = 0;
+			int32_t horizonal_resolution = 0;
+			int32_t vertical_resolution = 0;
+			uint32_t table_entries = 0;
+			uint32_t important_colors = 0;
+		};
+	};
+#pragma pack(pop)
+
+	// The reason it's gray is cos easier to debug your own software
+	// You must use clear_pixel() to set it black
+	union Col8 {
+		struct {
+			uint8_t r;
+			uint8_t	g;
+			uint8_t b;
+			uint8_t a;
+		};
+		uint32_t rgba = 0x202020ff;
+	};
+
+	const char BM_signature[2] = {'B', 'M'};
+
+	Header header;
+	Header::Info info;
+	std::vector<std::vector<Col8>> pixels;
+
+	int32_t data_cursor_x = 0;
+	int32_t data_cursor_y = 0;
+
+	Bmp(int32_t new_width, int32_t new_height) {
+		resize(new_width, new_height);
+	}
+
+	void resize(int32_t new_width, int32_t new_height) {
+		info.width = new_width;
+		info.height = new_height;
+		header.size = get_width() * get_height() + 54;
+
+		pixels.resize(get_width());
+		for (int32_t x = 0; x < get_width(); x++) {
+			pixels[x].resize(get_height());
+			for (int32_t y = 0; y < get_height(); y++) {
+				set_pixel(x, y, 0x20);
+			}
+		}
+	}
+
+	int32_t get_width() { return info.width; }
+	int32_t get_height() { return info.height; }
+	Col8 get_pixel(int32_t x, int32_t y) { return pixels[x][y]; }
+	Col8 *get_pixel_reference(int32_t x, int32_t y) { return &pixels[x][y]; }
+	Col8 *px_ptr(int32_t x, int32_t y) { return get_pixel_reference(x, y); }
+
+	void set_pixel(int32_t x, int32_t y, uint8_t gray) {
+		pixels[x][y] = Col8{gray, gray, gray};
+	}
+
+	void set_pixel(int32_t x, int32_t y, Col8 col) {
+		pixels[x][y] = col;
+	}
+
+	void set_pixel(
+		int32_t x, int32_t y,
+		uint8_t red, uint8_t green, uint8_t blue,
+		uint8_t alpha = 0
+	) {
+		pixels[x][y] = {red, green, blue, pixels[x][y].a};
+	}
+
+	Bmp &set_data_cursor_position(int32_t new_x, int32_t new_y) {
+		data_cursor_x = new_x;
+		data_cursor_y = new_y;
+		return *this;
+	}
+
+	Bmp &xy(int32_t x, int32_t y) {
+		return set_data_cursor_position(x, y);
+	}
+
+	Bmp &r(uint8_t new_red) {
+		pixels[data_cursor_x][data_cursor_y].r = new_red;
+		return *this;
+	}
+
+	Bmp &g(uint8_t new_green) {
+		pixels[data_cursor_x][data_cursor_y].g = new_green;
+		return *this;
+	}
+
+	Bmp &b(uint8_t new_blue) {
+		pixels[data_cursor_x][data_cursor_y].b = new_blue;
+		return *this;
+	}
+
+	Bmp &a(uint8_t new_alpha) {
+		pixels[data_cursor_x][data_cursor_y].a = new_alpha;
+		return *this;
+	}
+
+	void clear_pixel() {
+		for (int32_t x = 0; x < get_width(); x++)
+			for (int32_t y = 0; y < get_height(); y++)
+				pixels[x][y] = {0, 0, 0};
+	}
+
+	bool read(std::string file_name) {
+		// IFStream Input File Stream
+		std::ifstream file(file_name, std::ios::binary);
+
+		if (not file) {
+			std::cerr << "Failed to open the file." << std::endl;
+			return false;
+		}
+
+		file.read((char *)&header, 14);
+		file.read((char *)&info, 40);
+
+		if (header.signature == BM_signature) {
+			std::cerr << "Not a BMP file." << std::endl;
+			return false;
+		}
+
+		file.seekg(header.pixel_data_offset);
+		for (int32_t x = 0; x < get_width(); x++)
+			for (int32_t y = 0; y < get_height(); y++)
+				file.read((char *)px_ptr(x, y), info.color_depth / 8);
+
+		file.close();
+		return true;
+	}
+
+	void save_sample(std::string file_name = "sample.bmp") {
+		for (int32_t x = 0; x < get_width(); x++)
+			for (int32_t y = 0; y < get_height(); y++)
+				*px_ptr(x, y) = {uint8_t(x + y), 50, uint8_t(y * 0.3)};
+
+		save(file_name);
+	}
+
+	void save(std::string file_name) {
+		// OFStream Output File Stream
+		std::ofstream file(file_name, std::ios::binary);
+
+		file.write((char *)&header, 14);
+		file.write((char *)&info, 40);
+
+		for (int32_t x = 0; x < get_width(); x++) {
+			for (int32_t y = 0; y < get_height(); y++) {
+				file.write((char *)px_ptr(x, y), 4);
+			}
+		}
+
+		file.close();
+	}
+};
+
+
+void unit_test() {
+	{
+		Bmp bmp(512, 512);
+
+		bmp.save_sample();
+
+		bmp.clear_pixel();
+		bmp.read("sample.bmp");
+		bmp.save("read.bmp");
+	}
+
+	for (int32_t count = 20; count <= 45; count += 1) {
+		Bmp bmp(count, count / 2);
+
+		for (int32_t x = 10; x < std::min(30, bmp.get_width()); x++)
+			for (int32_t y = 10; y < std::min(30, bmp.get_height()); y++)
+				bmp.xy(x, y)
+				.r(x)
+				.g(y * 10)
+				.b(x * 30);
+
+		bmp.save(std::to_string(count) + ".bmp");
+	}
+}
+
+
+int main() {
+	unit_test();
+}
